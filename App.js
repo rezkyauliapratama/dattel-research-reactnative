1e3185c5165d095a6eef3d1d8eafa2ca5ba08d24
Rezky Aulia Pratama <rezkyauliapratama@gmail.com>
2018-10-14 21:04:12 +0700

    Revert "add list item"
    This reverts commit 6f2c4bad178995247121aa06d77e630e2fd2859b.

--- App.js
+++ App.js
@@ -1,53 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import ListItem from './src/components/ListItem/ListItem';
- 
+import { StyleSheet, Text, View } from 'react-native';
+
 export default class App extends React.Component {
-
-  state = {
-    placeName:"",
-    places:[]
-  };
-
-  placeNameChangedHandler = val => {
-    this.setState({
-      placeName: val
-    }); 
-  };
-
-  placeSubmitHandler = () => {
-      if (this.state.placeName.trim() === ""){
-        return;
-      }
-      this.setState(prevState => {
-        return {
-          places: prevState.places.concat(prevState.placeName)   
-         };
-      });
-  };
-
   render() {
-    const placesOutput = this.state.places.map((place,i) => (
-      <ListItem key={i} placeName={place}/>
-    ));
     return (
       <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput 
-            placeholder = "An awesome place"
-            value={this.state.placeName} 
-            onChangeText={this.placeNameChangedHandler}
-            style={styles.placeInput}
-          />
-          
-          <Button 
-            title="Add"
-            style={styles.placeButton}
-            onPress={this.placeSubmitHandler}/>
-
-
-        </View>
-        <View style={styles.placeOutputContainer}>{placesOutput}</View>
+        <Text>Hi guys , welcome to my first react native app</Text>
       </View>
     );
   }
@@ -56,25 +14,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 26,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'flex-start'
+    justifyContent: 'center',
   },
-  inputContainer: {
-    //flex:1,
-    width: "100%", 
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  placeInput:{
-    width: "70%"
-  },
-  placeButton : {
-    width: "30%"
-  },
-  placeOutputContainer:{
-    width: "100%",
-  }
 });
